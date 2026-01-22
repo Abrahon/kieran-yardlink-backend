832c0757bef4d5ce820651bc60e40b78da2e76a1
Sujon <mdabrahon926@gmail.com>
2026-01-23 05:12:09 +0600

    added connections model

--- connections/models.py
+++ connections/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from accounts.models import User
+
+class ConnectionRequest(models.Model):
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="sent_requests"
+    )
+    receiver = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="received_requests"
+    )
+
+    is_accepted = models.BooleanField(null=True)  
+    # None = pending | True = accepted | False = rejected
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("sender", "receiver")
+
+    def __str__(self):
+        return f"{self.sender} → {self.receiver}"
