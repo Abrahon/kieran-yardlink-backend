f7881e925410592c07a725608ef79eef9ddb798e
Sujon <mdabrahon926@gmail.com>
2026-01-29 01:12:54 +0600

    fixed issue qr code

--- qr/serializers.py
+++ qr/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from landscapers.models import LandscaperProfile, WorkingHours
 from services.models import Service
+from profiles.models import LandscaperProfilies
 
 
 class PublicServiceSerializer(serializers.ModelSerializer):
@@ -18,14 +19,17 @@ class WeeklyAvailabilitySerializer(serializers.ModelSerializer):
 
 
 class PublicLandscaperSerializer(serializers.ModelSerializer):
+    # from accounts.User
+    user_id = serializers.IntegerField(source="user.id", read_only=True)
     name = serializers.CharField(source="user.name", read_only=True)
     email = serializers.EmailField(source="user.email", read_only=True)
+    address = serializers.CharField(source="user.address", read_only=True)
+
+    # from profiles app
+    phone = serializers.SerializerMethodField()
     image = serializers.SerializerMethodField()
 
-    services = PublicServiceSerializer(
-        many=True,
-        read_only=True
-    )
+    services = PublicServiceSerializer(many=True, read_only=True)
 
     weekly_schedule = WeeklyAvailabilitySerializer(
         source="weekly_availability",
@@ -36,19 +40,36 @@ class PublicLandscaperSerializer(serializers.ModelSerializer):
     class Meta:
         model = LandscaperProfile
         fields = [
+            "user_id",
             "image",
             "name",
             "email",
-            "company_name",
             "phone",
             "address",
+            "business_name",
             "latitude",
             "longitude",
             "services",
             "weekly_schedule",
         ]
+        
+    # Helper methods
+
+    def get_profile(self, obj):
+        """
+        Fetch LandscaperProfilies safely
+        """
+        try:
+            return LandscaperProfilies.objects.get(user=obj.user)
+        except LandscaperProfilies.DoesNotExist:
+            return None
+
+    def get_phone(self, obj):
+        profile = self.get_profile(obj)
+        return profile.phone if profile else None
 
     def get_image(self, obj):
-        if obj.image:
-            return obj.image.url
+        profile = self.get_profile(obj)
+        if profile and profile.image:
+            return profile.image.url
         return None
